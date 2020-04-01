f8d4199b14103e0723a77a5e34b99f22051149f1
Rocío Reina Delgado <rocioreinadlg@gmail.com>
2020-04-01 12:17:07 +0200

    proyecto acabado

---
+++ src/main/java/Copyright.java
@@ -0,0 +1,14 @@
+public class Copyright {
+    public Copyright(){
+
+    }
+    public String devolverCopyright(){
+       return "Copyright (C) 1883 Rocio Reina - All Rights Reserved\n" +
+               "        You may use, distribute and modify this code under the\n" +
+               "        terms of the XYZ license, which unfortunately won't be\n" +
+               "        written for another century.\n" +
+               "\n" +
+               "        You should have received a copy of the XYZ license with\n" +
+               "        this file. If not, please visit : http://www.thomas-edison.com/" ;
+    }
+}
